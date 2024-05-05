3a0bd8d77a0e156f1061cdd30eabbf7a154d7dd1
siddhant ota <siddhantota@protonmail.com>
2024-05-05 09:03:26 +0530

    bubble sort done

--- bubblesort.js
+++ bubblesort.js
@@ -1,15 +1,17 @@
 const arr = [13, 46, 24, 52, 20, 9]
+let didSwap = 0
 
-for (let i = arr.length - 1; i > 0; i++) {
-	for(let j = 0; j < arr.length - 1; j++) {
-	
-		if (arr[j] > arr[j+1]) {
-			let temp = arr[j]
-			arr[j] = arr[j+1]
-			arr[j+1] = temp
-		}
+for (let i = arr.length - 1; i >= 0; i--){
+	 for (let j = 0; j < i; j++) {
+		 if ( arr[j] > arr[j+1] ){
+			const temp = arr[j]
+			arr[j] = arr[j + 1]
+			arr[j + 1] = temp
+			didSwap = 1
+		 }	
+	 }
 
-	}
+	if (didSwap === 0) break;
 }
 
 console.log(arr)
