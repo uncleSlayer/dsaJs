de6eb3a0ba2d37fcbd2815be66c41ad467e973f8
siddhant ota <siddhantota@protonmail.com>
2024-05-13 15:50:30 +0530

    delete head in a doubly linked list

---
+++ linked-list/doubly/doublyLinkedList.js
@@ -0,0 +1,28 @@
+const arr = [1, 2, 3, 4, 5, 6]
+
+class NodeList {
+
+    constructor(data) {
+
+        this.data = data
+        this.next = null
+        this.prev = null
+
+    }
+
+}
+
+const head = new NodeList(arr[0])
+let mover = head
+
+for (let i = 1; i <= arr.length - 1; i++) {
+
+    const temp = new NodeList(arr[i])
+    mover.next = temp
+    temp.prev = mover
+
+    mover = mover.next
+
+}
+
+console.log(head);
